5d8b2b02f5e8b26d6e0391e754d62ce653bc38f0
Anistratov Dmitry <anistratovd88@gmail.com>
2021-10-16 17:24:00 +0300

    Add project files.

---
+++ ModelingSystem/Indicator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingSystem
+{
+    public class Indicator
+    {
+        public int NumInterruptMessages { get; set; }
+        public int NumSpareChannel { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Число прерванных сообщений: {0,7}, Количество включений запасного канала: {1,7}",
+                NumInterruptMessages, NumSpareChannel);
+        }
+    }
+}
